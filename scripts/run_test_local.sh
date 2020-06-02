4c6cfaca20b60d2a4ea21852f06891380cafb8ab
Hielke Kramer <kramerh@gmail.com>
2020-06-02 18:13:32 +0200

    Added wabo source including stunnel for client side certs

--- scripts/run_test_local.sh
+++ scripts/run_test_local.sh
@@ -15,6 +15,15 @@ echo ""
  ./config/delegates_test.rb 'edepot:SA-00702-SA00632608_00001.jpg' "https://bwt.uitplaatsing.hcp-a.basis.lan/rest/SA/00702/SA00632608_00001.jpg"
  echo ""
 
+ echo "## wabo resolution"
+ ./config/delegates_test.rb 'wabo:/SDC/DOCUMENTUM/PRIMARY/27/0901B69980392066.PDF' "http://127.0.0.1:50000/webDAV/SDC/DOCUMENTUM/PRIMARY/27/0901B69980392066.PDF"
+ echo ""
+
+ echo "## wabo resolution with backslashes"
+ ./config/delegates_test.rb 'wabo:\\SDC\\DOCUMENTUM\\PRIMARY\\27\\0901B69980392066.PDF' "http://127.0.0.1:50000/webDAV/SDC/DOCUMENTUM/PRIMARY/27/0901B69980392066.PDF"
+ echo ""
+
+
  echo "## beeldbank resolution"
  ./config/delegates_test.rb beeldbank:B00000030938 "https://beeldbank.amsterdam.nl/component/ams_memorixbeeld_download/?format=download&id=B00000030938"
  echo ""
